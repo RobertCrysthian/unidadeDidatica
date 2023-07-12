a6e82d995270fe8b77e181f46c39aeab788e7138
robert crysthian <kedadefps@gmail.com>
2023-07-12 10:56:05 -0300

    ava

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import img from "./assets/img1.png"
 import img2 from "./assets/img2.png"
 import img3 from "./assets/img3.png"
 import img4 from "./assets/img4.png"
+import hq1 from "./assets/hq1.png"
+import hq2 from "./assets/hq2.png"
+import hq3 from "./assets/hq3.png"
+import hq4 from "./assets/hq4.png"
+import hq5 from "./assets/hq5.png"
+import hq6 from "./assets/hq6.png"
+import hq7 from "./assets/hq7.png"
+import hq8 from "./assets/hq8.png"
+import hq9 from "./assets/hq9.png"
+import hq10 from "./assets/hq10.png"
+import hq11 from "./assets/hq11.png"
+import hq12 from "./assets/hq12.png"
+import hq13 from "./assets/hq13.png"
 import Textos from './modules/Textos';
 
 
@@ -18,13 +31,13 @@ function App() {
     <Textos texto={<p>____Visamos com este trabalho criar uma unidade pedagógica que consiga instigar seus usuários a refletirem e conhecerem diversos elementos presente nas mídias que geram significados. Dentre esses elementos destacamos a presença dos códigos técnicos e simbólicos. Onde o primeiro está relacionado aos artifícios técnicos que geram significados(como ângulos de câmera e a música de um filme), e o segundo relacionado aos símbolos presentes nas mídias ( a presença de uma cor ou um gesto para representar algo, por exemplo).</p>}/>
     <Textos texto={
     <div>
-        <p>Você já parou para pensar em como os filmes trabalham com diversos recursos responsáveis por nos fazer reagir de uma determinada maneira? Por exemplo, um filme de terror vai gradativamente aumentando a música para gerar uma sensação de desconforto e insegurança, e um filme de ação usa ângulos de câmera exagerados para enfatizar uma determinada cena e aumentar seu impacto.</p>
+        <p>Você já parou para pensar em como os filmes trabalham com diversos recursos responsáveis por nos fazer reagir de uma determinada maneira? Por exemplo, um filme de terror vai gradativamente aumentando a música para gerar uma sensação de desconforto e insegurança, Ou um filme de drama pode deixar a câmera próxima do personagem para enfatizar sua dor.</p>
         <p>É importante saber que as mídias não se comunicam apenas através da linguagem verbal, diversos elementos não verbais também são responsáveis por gerar significados. Alguns desses elementos são: sons, gestos, signos, recursos  técnicos, figurino, estilização, etc.</p>
         <p>Dentro desta unidade você vai conhecer um pouco mais sobre esses recursos e vai aplicar seus conhecimentos com vários  exercícios.
         </p>
     </div>
   }/>
-    <Textos texto={"Para começar analise as imagens abaixo: "}/>
+    <Textos texto={<p>Para começar analise as imagens abaixo: </p>}/>
     <Textos texto={
     <div>
         <div>
@@ -47,7 +60,8 @@ function App() {
   />
     <Textos texto={
       <div>
-        <p>Vamos analisar agora o trecho de uma animação chamada a voz do silêncio:</p>
+        <p>Koe no katachi é uma animação que conta a história de Shoya Ishida, um estudante que, em sua inocência infantil, acabou fazendo bullying com uma colega surda de sua turma, a Nishimiya. Suas ações eram encorajadas pela turma e foram tão longe que acabaram trazendo diversos problemas para ambos os personagens. Este evento prejudicou tanto Ishida que gerou consequências durante toda sua adolescência, sendo a exclusão social uma delas.</p>
+        <p>Vamos analisar agora o trecho da cena final do filme:</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Hmrfas01C_s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     }/>
@@ -72,9 +86,11 @@ function App() {
           <p>04 - (pergunta pessoal). Podemos sim inferir que o gênero do filme é drama ou algo parecido, pois essa é uma música que geralmente encontramos nesse tipo de filme.</p>
         </div>
       }/>
+
     <Textos texto={
       <div>
-        <p>assista a cena do filme sound of metal e responda em seguida:</p>
+        <p>Sound of Metal é um filme que apresenta um baterista de uma pequena banda que vai perdendo a audição. Inicialmente não aceita sua nova situação, passando por diversos eventos que vão mudar sua vida para sempre.</p>
+        <p>Assista a cena do filme e responda em seguida:</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Hm00Mk5dE3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>}/>
     <Perguntas pergunta={
@@ -82,17 +98,58 @@ function App() {
         <p> 01 - Nessa cena em questão conseguimos ter uma imersão muito grande com os personagens do filme através de um recurso específico. Que recurso é esse? Como ele contribui para que possamos nos sentir como os personagens?</p>
         <input></input>
         <p> 02 - Você acha que esse trecho teria a mesma sensação e sentido se a parte sonora dele fosse diferente? Por quê?</p>
+        <input></input>
+        <p> 03 - O que você sente presenciando essa cena? Acha que ela conseguiu passar as sensações de maneira eficiente?</p>
       </div>
     }resposta={
       <div>
         <p> 01 - É o recurso sonoro. Através dele o filme busca uma maneira de representar como esses personagens ouvem as coisas, e com isso conseguimos nos conectar profundamente com eles.</p>
         <p> 02 - Não. Os efeitos sonoros são essenciais para que seja passada a sensação de estar no corpo do personagem e ouvir o mundo do mesmo modo que ele. Sem os efeitos sonoros tudo isso é perdido, juntamente com o impacto da cena.</p>
+        <p> 03 - Resposta Pessoal.</p>
       </div>
     }/>
+
+<Textos texto={
+        <div>
+          <p>Trouxemos abaixo alguns quadros de Ava’s Demon, uma HQ gratuita que se encontra disponível para leitura no seguinte link: https://www.avasdemon.com/</p>
+          <p>Esta obra nos apresenta Ava, uma personagem que vive em uma espécie de mundo futurista com um demônio preso em seu corpo. Esta sendo uma rainha que, para evitar morrer em uma derrota iminente de seu rival, tomou uma poção e reencarnou no corpo de nossa protagonista. Este demônio é capaz de controlar periodicamente o corpo de Ava, importunando-a por não aceitar seu pacto.</p>
+        </div>
+      }/>
+<Textos texto={
+  <div>
+    <img src={hq1} width={400} height={300}/>
+    <img src={hq2} width={400} height={300}/>
+    <img src={hq3} width={400} height={300}/>
+    <img src={hq4} width={400} height={300}/>
+  </div>
+}/>
+
+<Textos texto={
+  <div>
+    <img src={hq5} width={400} height={300}/>
+    <img src={hq6} width={400} height={300}/>
+    <img src={hq7} width={400} height={300}/>
+    <img src={hq8} width={400} height={300}/>
+  </div>
+}/>
+
+<Textos texto={
+  <div>
+    <img src={hq9} width={400} height={300}/>
+    <img src={hq10} width={400} height={300}/>
+    <img src={hq11} width={400} height={300}/>
+    <img src={hq12} width={400} height={300}/>
+    <img src={hq13} width={400} height={300}/>
+  </div>
+}/>
+
     <Textos texto={
     <div>
       <p>Não apenas a música e angulação nos diz sobre o filme, como também o filtro, ou coloração presente nas cenas. Observe os trechos a seguir:</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Hm00Mk5dE3Ee" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/V4qwn4aoPU8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/HvWx0flvdxw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bunz_oFfJ-8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/-zJOKWEauN4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>}/>
 
     <Perguntas pergunta={<p>Agora reflita como a coloração da cena interfere no que sentimos sobre ela, e as sensações que cada cena transmite com isso.</p>}/>
@@ -100,15 +157,15 @@ function App() {
     <Textos texto={
       <div>
         <p>Podemos ver características filmográficas não apenas em filmes, mas também em clipes de música, que muitas vezes transmitem uma ideia de história. Observe o clipe a seguir.</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/cMFHUTJ13Yse" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cMFHUTJ13Ys" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>}/>
     <Perguntas pergunta={<p>Ainda pensando na questão anterior, responda: o que a mudança na cor do clipe pode nos dizer acerca do que eles querem passar com a “história” do clipe?</p>}/>
   
     <Textos texto={
       <div>
         <p>Os clipes a seguir trazem consigo algumas ideias semelhantes em sua filmografia, assista a ambos e nos diga o que você consegue absorver da “história”.</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/cMFHUTJ13Yse" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/cMFHUTJ13Yse" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FQRnJvbLTAo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/r5GaAEHvHj0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     }/>
     <Perguntas pergunta={
